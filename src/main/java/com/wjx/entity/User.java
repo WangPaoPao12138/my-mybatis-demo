cb12ddd6869220c27da301bbb525edc0fede3783
wjx_Gin <1162547771@qq.com>
2023-08-30 11:34:30 +0800

    init mybatis org demo

---
+++ src/main/java/com/wjx/entity/User.java
@@ -0,0 +1,41 @@
+package com.wjx.entity;
+
+/**
+ * @author Gin
+ * @description
+ * @date 2023/8/30 11:27
+ */
+public class User {
+    private Integer id;
+
+    private String name;
+
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
